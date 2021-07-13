bca2c3996a4adeeb316a6295ddd53b17f6294da4
Muskaan Singh <muskaansingh235@gmail.com>
2021-07-13 10:45:12 +0530

    added API and display data in the console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
+import axios from 'axios';
 
 export default class App extends Component {
+
+  state = {
+    quote: ' '
+  }
+
+  componentDidMount() {
+    this.fetchQuotes();
+  }
+
+  fetchQuotes = () => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,3 +30,4 @@ export default class App extends Component {
     )
   }
 }
+
